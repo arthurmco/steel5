f15639f43494de4f31df1c9793075b3060234601
Arthur Mendes <arthurmco@gmail.com>
2020-03-01 18:32:44 -0300

    Start decoding instructions
    Decodes all instructions but the branch ones

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-
+use num_derive::{ToPrimitive, FromPrimitive};
+use num_traits::{ToPrimitive, FromPrimitive};
 
 /*
  * Represents a CPU state
@@ -25,28 +26,28 @@
   */
 #[derive(Debug)]
 pub enum InstructionType {
-    RInst{rd: u16, funct3: u16, rs1: u16, rs2: u16, funct7: u16},
-    IInst{rd: u16, funct3: u16, rs1: u16, imm: u32},
-    SInst{rd: u16, funct3: u16, rs1: u16, rs2: u16, imm: u32},
+    RInst{rd: u32, funct3: u32, rs1: u32, rs2: u32, funct7: u32},
+    IInst{rd: u32, funct3: u32, rs1: u32, imm: i32},
+    SInst{rd: u32, funct3: u32, rs1: u32, rs2: u32, imm: i32},
 
     // B-type has the same fields as S-tyle, but the immediate
     // is spread in memory like this:
     // | imm[12] | imm[10:5] | rs2 | rs1 | funct3 | imm[4:1] | imm[11] | opcode
-    BInst{rd: u16, funct3: u16, rs1: u16, rs2: u16, imm: u32},
+    BInst{rd: u32, funct3: u32, rs1: u32, rs2: u32, imm: u32},
 
     // Watch out that we only save the top 20 bits of the immediate
     // in the U-instruction
-    UInst{rd: u16, imm: u32},
+    UInst{rd: u32, imm: i32},
 
     // J-type has the same fields as I type, but the immediate is
     // spread like this, in memory
     // | imm[20] | imm[10:1] | imm[11] | imm[19:12] | rd | opcode |
-    JInst{rd: u16, imm: u32}
+    JInst{rd: u32, imm: u32}
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, ToPrimitive, FromPrimitive)]
 pub enum OpcodeImmFunctions {
-    value = 0b0010011,
+    Value = 0b0010011,
 
     // Adds imm to value in register rs1
     ADDI = 0b000,
@@ -78,9 +79,9 @@ pub enum OpcodeImmFunctions {
     SRLI_SRAI = 0b101
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, ToPrimitive, FromPrimitive)]
 pub enum OpcodeRegFunctions {
-    value = 0b0110011,
+    Value = 0b0110011,
 
     // Adds rs1 to rs2, puts result into rd
     // Subtracts rs2 from rs1
@@ -116,9 +117,9 @@ pub enum OpcodeRegFunctions {
 // All branch instructions, when branching, will get the
 // 12-bit offset from the immediate, sign extend it, add
 // to pc and jump to it.
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, ToPrimitive)]
 pub enum BranchFunctions {
-    value = 0b1100011,
+    Value = 0b1100011,
 
     // Branch if rs1 and rs2 are equal.
     BEQ = 0,
@@ -140,7 +141,7 @@ pub enum BranchFunctions {
 
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, ToPrimitive)]
 pub enum FreeOpcodes {
     
     /**** I-type ****/
@@ -152,6 +153,13 @@ pub enum FreeOpcodes {
     // exception of the instruction is not aligned to a
     // 4-byte boundary
     JALR = 0b1100111,
+
+    // Loads a value from memory address (rs1 + offset) to rd
+    // funct3 stores the load width (0=byte, 1=word, 2=dword)
+    // The MSB means that the load is unsigned.
+    // The byte, halfword and word load mnemonics are LB, LH and LW, the unsigned
+    // variants are LBU, LHU and LWU
+    LOAD = 0b0000011,
 
     /**** S-type ****/
     
@@ -181,14 +189,6 @@ pub enum FreeOpcodes {
     // (pc+4) into rd. Usually rd is x1.
     // Unconditional jumps (j <offset>) are encoded as "jal x0, <offset>"
     JAL = 0b1101111,
-
-    // Loads a value from memory address (rs1 + offset) to rd
-    // funct3 stores the load width (0=byte, 1=word, 2=dword)
-    // The MSB means that the load is unsigned.
-    // The byte, halfword and word load mnemonics are LB, LH and LW, the unsigned
-    // variants are LBU, LHU and LWU
-    LOAD = 0b0000011,
-
 }
 
 /*
@@ -214,35 +214,181 @@ pub enum Opcode {
     FREE_OPS(FreeOpcodes)
 }
 
- /*
-  * A RISC-V instruction
-  *
-  * This includes the opcode plus some additional registers that it uses,
-  * or some memory offset, depending on the opcode.
-  * We also have fields in the instruction for signaling a future variable-length
-  * encoding.
-  *
-  * All instructions must be aligned on a 32-bit boundary. If the instruction is
-  * variable-length, it length must be a multiple of 16-bit.
-  *
-  * Because of this scheme, all 32-bit RISC-V instructions have the 2-lower order bits
-  * set.
-  *
-  * Also, instructions with the lower 16-bits all zeroed are invalid
-  */
-  #[derive(Debug)]
-  pub struct Instruction {
-     opcode: u32,
-     data: InstructionType
-  }
+/*
+ * A RISC-V instruction
+ *
+ * This includes the opcode plus some additional registers that it uses,
+ * or some memory offset, depending on the opcode.
+ * We also have fields in the instruction for signaling a future variable-length
+ * encoding.
+ *
+ * All instructions must be aligned on a 32-bit boundary. If the instruction is
+ * variable-length, it length must be a multiple of 16-bit.
+ *
+ * Because of this scheme, all 32-bit RISC-V instructions have the 2-lower order bits
+ * set.
+ *
+ * Also, instructions with the lower 16-bits all zeroed are invalid
+ */
+ #[derive(Debug)]
+ pub struct Instruction {
+    opcode: Opcode,
+    data: InstructionType
+ }
   
-  impl Instruction {
-      fn new(inst: u32) -> Instruction {
-          return Instruction{opcode: 0, data: InstructionType::UInst{rd:0, imm:0}}
-      }
+impl Instruction {
+
+    /* Parse register and immediate fields for a i-type instruction */
+    fn parse_itype(inst: u32) -> InstructionType {
+        let rd = (inst >> 7) & 0x1f;
+        let funct3 = (inst >> 12) & 0x7;
+        let rs1 = (inst >> 15) & 0x1f;
+        let uimm = (inst >> 20) & 0x7ff;
+
+        // Immediates are signed!
+        let imm : i32 =  if uimm > 1024 { 
+            (uimm as i32) - 2048 
+        } else {uimm as i32};
+
+        InstructionType::IInst{rd, funct3, rs1, imm}
+    }
+
+    /* Parse register and immediate fields for a S-type instruction */
+    fn parse_stype(inst: u32) -> InstructionType {
+        let rd = (inst >> 7) & 0x1f;
+        let funct3 = (inst >> 12) & 0x7;
+        let rs1 = (inst >> 15) & 0x1f;
+        let rs2 = (inst >> 20) & 0x1f;
+        let uimm = (inst >> 25) & 0x3f;
+
+        // Immediates are signed!
+        let imm : i32 =  if uimm > 32 { 
+            (uimm as i32) - 64 
+        } else {uimm as i32};
+
+        InstructionType::SInst{rd, funct3, rs1, rs2, imm: imm << 5}
+    }
+
+    /* Parse register and immediate fields for a R-type instruction */
+    fn parse_rtype(inst: u32) -> InstructionType {
+        let rd = (inst >> 7) & 0x1f;
+        let funct3 = (inst >> 12) & 0x7;
+        let rs1 = (inst >> 15) & 0x1f;
+        let rs2 = (inst >> 20) & 0x1f;
+        let funct7 = (inst >> 25) & 0x3f;
+
+        InstructionType::RInst{rd, funct3, rs1, rs2, funct7}
+    }
+
+    /* Parse register and immediate fields for a U-type instruction */
+    fn parse_utype(inst: u32) -> InstructionType {
+        let rd = (inst >> 7) & 0x1f;
+        
+        let uimm = (inst >> 12) & 0xfffff;
+
+        // Immediates are signed!
+        let imm : i32 =  if uimm > 0x80000 { 
+            (uimm as i32) - 0x100000 
+        } else {uimm as i32};
+
+        InstructionType::UInst{rd, imm: imm << 12}
+    }
+
+    /**
+     * Decode a instruction from a 4-bit integer
+     *
+     * This function will have to be adapted for variable length instructions
+     */
+    fn new(inst: u32) -> Instruction {
+        let opcode_val = inst & ((1 << 6) - 1);
+        let funct3 = (inst >> 12) & 0x7;
+
+        let ops = [
+            ToPrimitive::to_u32(&OpcodeRegFunctions::Value).unwrap(),
+            ToPrimitive::to_u32(&OpcodeImmFunctions::Value).unwrap(),
+            ToPrimitive::to_u32(&BranchFunctions::Value).unwrap(),
+            ToPrimitive::to_u32(&FreeOpcodes::JALR).unwrap(),
+            ToPrimitive::to_u32(&FreeOpcodes::STORE).unwrap(),
+            ToPrimitive::to_u32(&FreeOpcodes::LUI).unwrap(),
+            ToPrimitive::to_u32(&FreeOpcodes::AUIPC).unwrap(),
+            ToPrimitive::to_u32(&FreeOpcodes::JAL).unwrap(),
+            ToPrimitive::to_u32(&FreeOpcodes::LOAD).unwrap(),
+        ];
+
+        let opcode : Opcode = match opcode_val {
+            _ if opcode_val == ops[0] => Opcode::OP(
+                match FromPrimitive::from_u32(funct3) {
+                    Some(OpcodeRegFunctions::ADD_SUB) => OpcodeRegFunctions::ADD_SUB,
+                    Some(OpcodeRegFunctions::SLT) => OpcodeRegFunctions::SLT,
+                    Some(OpcodeRegFunctions::SLTU) => OpcodeRegFunctions::SLTU,
+                    Some(OpcodeRegFunctions::AND) => OpcodeRegFunctions::AND,
+                    Some(OpcodeRegFunctions::OR) => OpcodeRegFunctions::OR,
+                    Some(OpcodeRegFunctions::XOR) => OpcodeRegFunctions::XOR,
+                    Some(OpcodeRegFunctions::SLL) => OpcodeRegFunctions::SLL,
+                    Some(OpcodeRegFunctions::SRL_SRA) => OpcodeRegFunctions::SRL_SRA,
+                    _ => panic!("Unknown funct3 value in OP_IMM")
+                }
+            ),
+            _ if opcode_val == ops[1] => Opcode::OP_IMM(
+                match FromPrimitive::from_u32(funct3) {
+                    Some(OpcodeImmFunctions::ADDI) => OpcodeImmFunctions::ADDI,
+                    Some(OpcodeImmFunctions::SLTI) => OpcodeImmFunctions::SLTI,
+                    Some(OpcodeImmFunctions::SLTIU) => OpcodeImmFunctions::SLTIU,
+                    Some(OpcodeImmFunctions::ANDI) => OpcodeImmFunctions::ANDI,
+                    Some(OpcodeImmFunctions::ORI) => OpcodeImmFunctions::ORI,
+                    Some(OpcodeImmFunctions::XORI) => OpcodeImmFunctions::XORI,
+                    Some(OpcodeImmFunctions::SLLI) => OpcodeImmFunctions::SLLI,
+                    Some(OpcodeImmFunctions::SRLI_SRAI) => OpcodeImmFunctions::SRLI_SRAI,
+                    _ => panic!("Unknown funct3 value in OP_IMM")
+                }),
+            _ if opcode_val == ops[2] => Opcode::BRANCH(BranchFunctions::Value),
+
+            _ if opcode_val == ops[3] => Opcode::FREE_OPS(FreeOpcodes::JALR),
+            _ if opcode_val == ops[4] => Opcode::FREE_OPS(FreeOpcodes::STORE),
+            _ if opcode_val == ops[5] => Opcode::FREE_OPS(FreeOpcodes::LUI),
+            _ if opcode_val == ops[6] => Opcode::FREE_OPS(FreeOpcodes::AUIPC),
+            _ if opcode_val == ops[7] => Opcode::FREE_OPS(FreeOpcodes::JAL),
+            _ if opcode_val == ops[8] => Opcode::FREE_OPS(FreeOpcodes::LOAD),
+            _ => panic!("Unrecognized opcode")
+        };
+
+        let instruction_type = match opcode {
+            Opcode::OP(ins) => Instruction::parse_rtype(inst),
+            Opcode::OP_IMM(ins) => Instruction::parse_itype(inst),
+            Opcode::FREE_OPS(ins) => {
+                match ins {
+                    FreeOpcodes::JALR => Instruction::parse_itype(inst),
+                    FreeOpcodes::STORE => Instruction::parse_stype(inst),
+                    FreeOpcodes::LUI => Instruction::parse_utype(inst),
+                    FreeOpcodes::AUIPC => Instruction::parse_utype(inst),
+                    FreeOpcodes::JAL => {
+                        // J-type
+                        let rd = (inst >> 7) & 0x1f;
+            
+                        let imm12 = (inst >> 12) & ((1 << 7) - 1);
+                        let imm11 = (inst >> 20) & 0x1;
+                        let imm1 = (inst >> 21) & ((1 << 9) - 1);
+                        let imm20 = (inst >> 31) & 0x1;
+
+                        let imm = (imm1 << 1) | (imm11 << 11) | (imm12 << 12) | (imm20 << 20);
+
+                        InstructionType::JInst{rd, imm}
+                    },
+                    FreeOpcodes::LOAD => Instruction::parse_itype(inst),
+                }
+            }
+            _ => panic!("Unknown instruction format")
+        };
+
+        Instruction{opcode, data: instruction_type}
+    }
   }
 
 
 fn main() {
     println!("Hello, world!");
+
+    println!("{:?}", Instruction::new(0xFFF28413)); //addi s0 t0 -1
+    println!("{:?}", Instruction::new(0x007302b3)); //add x5, x6, x7
+    println!("{:?}", Instruction::new(0x00512a03)); //lw s4 5(sp)
 }
